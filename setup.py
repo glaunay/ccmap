72fd9c556eb574295d2edfb1614380dc831966c9
Juliette MARTIN <jmartin@arwen.ibcp.fr>
2019-05-02 15:08:28 +0200

    ajout des dependances pour compilation sur arwen

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from distutils.core import setup, Extension
+import os
+import sysconfig
 
+extra_compile_args = sysconfig.get_config_var('CFLAGS').split()
+extra_compile_args += ['-D', 'AS_PYTHON_EXTENSION', '-std=c99', '-pedantic']
+extra_link_args = ['-L/home/glaunay/python3.6/lib']
 module1 = Extension('ccmap',
                     libraries = ['m'],
-                    include_dirs = ['./include'],
+                    include_dirs = ['./include', '/home/glaunay/python3.6/include/python3.6m'],
                     sources = ['ccmapmodule.c', './src/mesh.c', './src/transform_mesh.c', './src/decoygen.c'],
-                    extra_compile_args=['-D', 'AS_PYTHON_EXTENSION', '-std=c99', '-pedantic'])
-                    #extra_compile_args=['-D', 'DEBUG', '-D', 'AS_PYTHON_EXTENSION', '-std=c99', '-pedantic'])
+                    #extra_compile_args=['-D', 'AS_PYTHON_EXTENSION', '-std=c99', '-pedantic', '-L/home/glaunay/python3.6/lib'])#, '-lpython3.6'])
+                    extra_compile_args=extra_compile_args,
+		    extra_link_args=extra_link_args
+			#extra_compile_args=['-D', 'DEBUG', '-D', 'AS_PYTHON_EXTENSION', '-std=c99', '-pedantic'])
+		)
 
 setup (name = 'ccmapModule',
        version = '1.0',
